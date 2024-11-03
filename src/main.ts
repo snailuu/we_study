bcf540b03eb639ebbf0d97c8734d8b34a9baaced
snailuu <67217277@qq.com>
2024-11-03 10:12:45 +0800

    fix: 俄罗斯方块思路

--- src/main.ts
+++ src/main.ts
@@ -1,103 +1,139 @@
-const mconsole = (function (
-	$c: typeof document.createElement,
-	$a: <T extends Node>(Sparent: Node, schild: T) => T,
-	$body: Document['body'],
-	$doc: Document,
-) {
-	function splitMessage(message: unknown): any[] {
-		if (typeof message == 'string') {
-			return message.split('\n');
+/**
+ * 名称： 俄罗斯方块游戏
+ *
+ * 功能：（游戏角度）
+ *    移动方块
+ *    旋转方块
+ *    渲染方块
+ *    创建方块
+ *    消除行
+ *    碰撞检测
+ *
+ * 流程：（用户交互角度）
+ *    开始游戏
+ *    监听用户的键盘方向键事件
+ *        控制方块移动和旋转
+ *        渲染方块
+ *    创建计时器
+ *        判断当前游戏状态
+ *            生成中
+ *              判断是否有完整行
+ *                  True: 消除当前完整行
+ *              生成不同形态的方块组合，将当前游戏状态改为【移动中】
+ *            移动中
+ *                方块下落
+ *                当方块底部接触到东西
+ *                    判断方块是否超出规定区域
+ *                        True: 将当前游戏状态改为【结束】, 清除计时器
+ *                        False: 将当前游戏状态改为【生成中】
+ *        渲染方块
+ *    卸载监听事件
+ *    游戏结束、记录分数
+ *
+ * 形状：
+ *  L  J
+ *  S  Z
+ *  I  O  T
+ */
+
+interface Block {
+	x: number;
+	y: number;
+}
+
+interface Shape {
+	data: Block[];
+	direction: number;
+	status: 'move' | 'end';
+}
+
+interface GameInfo {
+	data: (Block | null)[][];
+	status: 'moving' | 'end' | 'create';
+	score: number;
+}
+
+const gameInfo: GameInfo;
+
+let currShape: Shape;
+
+/** 创建形状 */
+function createShape(): Shape;
+
+/** 移动方块 */
+function move(direction: 'left' | 'right' | 'down'): void;
+
+/** 旋转方块 */
+function rotate(): void;
+
+/** 碰撞检测 */
+function checkNoMove(type: 'down' | 'top' | 'left' | 'right' = 'down'): boolean;
+
+/** 旋转检测 */
+function checkNoRotate(): boolean;
+
+/** 渲染 */
+function render(): void;
+
+/** 消除行 */
+function removeRow(index: number): void;
+
+/** 获取完整行 */
+function getFullRows(): number[];
+
+window.addEventListener('keypress', () => {
+	// 移动
+	if (!(checkNoMove(/** 方向 */))) move(/** 方向 */);
+	// 旋转
+	if (!checkNoRotate()) rotate();
+	render();
+});
+
+let timer = setInterval(() => {
+	if (gameInfo.status === 'create') {
+		const rows = getFullRows();
+		if (rows.length) {
+			for (const row of rows) {
+				removeRow(row);
+			}
 		}
-		if (typeof message === 'object') {
-			return [JSON.stringify(message)];
+		createShape();
+		gameInfo.status = 'moving';
+	} else {
+		move('down');
+		if (checkNoMove()) {
+			if (checkNoMove('top')) {
+				clearInterval(timer);
+				gameInfo.status = 'end';
+				// 记录分数等
+			}
+			gameInfo.status = 'create';
 		}
-		return [message];
 	}
+});
 
-	function ignoreStyle(style: string): string {
-		const supportStyleProps =
-			/((background|border|box-shadow|box-decoration-break|clear|float|color|cursor|display|font|line-height|margin|outline|padding|position|text-align|text-decoration|text-shadow|transform|transition|vertical-align|white-space).*?;)/g;
-		let styleText = '';
-		for (const [styleProp] of style.matchAll(supportStyleProps)) {
-			styleText += styleProp;
-		}
-		return styleText;
-	}
+import { curry } from '@cmtlyt/base';
 
-	function formatSplit(
-		message: unknown,
-		...args: unknown[]
-	): { messages: string[]; style?: string }[] {
-		if (typeof message === 'string' && message.includes('%c')) {
-			const messages = message.split('%c');
-			return ([] as { messages: string[]; style?: string }[]).concat(
-				messages[0] ? { messages: splitMessage(messages[0]) } : [],
-				messages.slice(1).map((message, index) => {
-					return {
-						messages: splitMessage(message),
-						style: ignoreStyle(args[index] as string),
-					};
-				}),
-			);
-		}
-		return [
-			{ messages: splitMessage(message) },
-			...args.map((item) => ({ messages: splitMessage(item) })),
-		];
-	}
+const temp = curry((a: number, b: string, c: boolean, d: number[]): boolean => {});
 
-	function $createMessage(messages: string[], style?: string) {
-		let needEnter = false;
-		return messages
-			.map((message, idx) => {
-				const $content = $c('div');
-				$content.innerHTML = `${message}${idx === messages.length - 1 ? '' : ''}`;
-				if (!message) {
-					if (needEnter) {
-						$content.dataset.ignore = 'true';
-						needEnter = false;
-					} else {
-						needEnter = true;
-					}
-				}
-				if (style) {
-					$content.style.cssText = `${style}; width: fit-content;`;
-				}
-				if (idx) {
-					$content.style.cssText += 'width: fit-content; overflow-wrap: anywhere;';
-				} else {
-					$content.style.cssText += 'display: inline-block;';
-				}
-				$content.style.cssText += `white-space: pre-wrap; word-break: break-all; ${
-					message ? '' : 'height: 0;'
-				}`;
-				return $content;
-			})
-			.filter(($dom) => !$dom.dataset.ignore);
-	}
+const a = temp()(1, '2')(true);
 
-	function messageHandler(...args: unknown[]) {
-		const formatted = formatSplit(args[0], ...args.slice(1));
-		return formatted.map(({ messages, style }) => {
-			const $wrapper = $doc.createDocumentFragment();
-			$createMessage(messages, style).forEach(($item) => $a($wrapper, $item));
-			return $wrapper;
-		});
-	}
+const b = a([1]);
 
-	function log(...args: unknown[]): void {
-		const $div = $c('div');
-		$div.style.cssText =
-			'max-width: 600px; width: max-content; overflow-x: hidden; padding: 10px 0; font-family: monospace;';
-		const $messages = messageHandler(...args);
-		$messages.forEach(($item) => $a($div, $item));
-		$a($body, $div);
-	}
+const routes = [
+	{
+		path: '',
+		component: () => import('@/index.html'),
+		children: [],
+	},
+];
 
-	return { log };
-})(
-	document.createElement.bind(document),
-	(sparent, $child) => sparent.appendChild($child),
-	document.body,
-	document,
-);
+{
+	flag ? (
+		<a />
+	) : ();
+}
+
+<show if={flag}>
+	<a />
+</show>;
